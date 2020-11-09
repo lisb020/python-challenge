b6bd7088aa08f7f88221701ce83964795e37fce4
Lisa Stroh <lisastroh@Lisas-MacBook-Air.local>
2020-11-08 23:03:47 -0800

    finished PyBank code

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,6 +5,9 @@ import csv
 # file path for csv
 csv_file = os.path.join("Resources", "budget_data.csv")
 
+# file path for text file to write to
+text_file = os.path.join("analysis", "budget_data.txt")
+
 # open csv file
 with open(csv_file, "r") as file:
     csvreader = csv.reader(file)
@@ -20,16 +23,25 @@ with open(csv_file, "r") as file:
     for row in csvreader:
         months += 1
         total += int(row[1])
-        profitloss.append(int(row[1]))
-
-    # loop through profitloss list to find difference
+        # put date and profit/loss in list
+        profitloss.append([row[0],int(row[1])])
+   
+    # loop through profit/loss element (1) of profitloss list to find difference
     for i in range(1,len(profitloss)):
-        profitlossdif.append(profitloss[i] - profitloss[i-1])
-    #for i in range(len(profitlossdif))
-        #if profitlossdif[i] > max:
-            #max = profitlossdif[i]
-    # find average diff
+        profitlossdif.append(profitloss[i][1] - profitloss[i-1][1])
+    
+    # find average of profitloss difference
     average = round(sum(profitlossdif)/len(profitlossdif),2)
+    
+    # find the max and min profit loss difference
+    maxpro = max(profitlossdif)
+    minpro = min(profitlossdif)
+
+    # find the max and min profit loss difference index and then use to find the date
+    maxindex = profitlossdif.index(maxpro)
+    maxdate = profitloss[maxindex+1][0]
+    minindex = profitlossdif.index(minpro)
+    mindate = profitloss[minindex+1][0]
     
     # print out results
     print("Financial Analysis")
@@ -37,7 +49,17 @@ with open(csv_file, "r") as file:
     print(f"Total Months: {months}")
     print(f"Total: ${total}")
     print(f"Average Change: ${average}")
-    print(f"Greatest Increase in Profits: ${max(profitlossdif)}")
-    print(f"Greatest Decrease in Profits: ${min(profitlossdif)}")
-    
+    print(f"Greatest Increase in Profits: {maxdate} (${maxpro})")
+    print(f"Greatest Decrease in Profits: {mindate} (${minpro})")
+
+    # write to text file
+    with open(text_file,'w') as txtfile:
+        txtfile.writelines(["Financial Analysis\n",
+                            "--------------------------------\n",
+                            "Total Months: " + str(months) + "\n",
+                            "Total: $" + str(total) + "\n",
+                            "Average Change: $" + str(average) + "\n",
+                            "Greatest Increase in Profits: " + str(maxdate) + " ($" + str(maxpro) + ")\n",
+                            "Greatest Decrease in Profits: " + str(mindate) + " ($" + str(minpro) + ")\n"])
+
     
